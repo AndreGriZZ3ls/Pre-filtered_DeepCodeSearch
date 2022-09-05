7322a074c63abc17c3d49b564e9eb2308b89480b
André Warnecke <andre.warnecke036@web.de>
2022-09-05 18:31:08 +0200

    Search with inverted index test 10

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ if __name__ == '__main__':
                     for line_nr in list:
                         cnt[line_nr] += 1
                 #min_common = len(query_list) / 2 + len(query_list) % 2
-                result_line_numbers = cnt.most_common(10000 + 100 * n_results)[0][0]
+                result_line_numbers = cnt.most_common(10000 + 100 * n_results)[:][0]
             else:
                 raise Exception(f'Unsupported index type: {index_type}')
             
