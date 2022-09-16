ac9f356ad12e73edfaeddfe4c4e638000148151e
André Warnecke <andre.warnecke036@web.de>
2022-09-16 13:43:53 +0200

    Fix for not enough potential results per thread 6

--- main.py
+++ main.py
@@ -174,9 +174,9 @@ if __name__ == '__main__':
                 codebase_lines = list(f(full_codebase)) ###### TODO: ohne multithreding ausprobieren ###############################
                 vector_lines   = list(f(full_code_reprs))
                 #chunk_size     = math.ceil(len(result_line_numbers) / n_results)
-                chunk_size     = n_results - 1
+                chunk_size     = n_results
                 
-                for i in range(0, len(codebase_lines), n_results):
+                for i in range(0, len(codebase_lines), chunk_size):
                     codebase.append(codebase_lines[i:i + chunk_size])
                     codereprs.append( vector_lines[i:i + chunk_size])
                 engine._code_reprs = codereprs
