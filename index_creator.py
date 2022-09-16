26ec4f150a0a99bd34f4d35a9fc39ce58b0bcb77
André Warnecke <andre.warnecke036@web.de>
2022-09-16 20:12:31 +0200

    Index [] test 1

--- index_creator.py
+++ index_creator.py
@@ -106,6 +106,7 @@ class IndexCreator:
             for i, line in enumerate(tqdm(lines)):
                 for word in line:
                     if word != '[]': continue
+                    print("[] located!")
                     if word in index:
                         index[word].append(i)
                     else:
