5adb693ee456e7903900997260065bf5cb03632e
André Warnecke <andre.warnecke036@web.de>
2022-09-26 20:39:41 +0200

    Efficiency comparison 18 - fix 9

--- DeepCSKeras/data_loader.py
+++ DeepCSKeras/data_loader.py
@@ -53,7 +53,9 @@ def load_code_reprs(path, chunk_size):
     codereprs = []
     #if chunk_size < 0: return np.array(tables.open_file(path).root.vecs)
     h5f  = tables.open_file(path, 'r')
-    if chunk_size < 0: return h5f.root.vecs
+    if chunk_size < 0: 
+        filters = tables.Filters(complib = 'blosc', complevel = 5)
+        return h5f.root.chunk_array
     vecs = h5f.root.vecs
     for i in tqdm(range(0, len(vecs), chunk_size)):
         codereprs.append(vecs[i:i + chunk_size])
