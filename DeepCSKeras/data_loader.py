081297136df29413a4d89cfd9d56ba932bc473e1
André Warnecke <andre.warnecke036@web.de>
2022-10-11 05:51:44 +0200

    evaluation data to database 57

--- DeepCSKeras/data_loader.py
+++ DeepCSKeras/data_loader.py
@@ -14,7 +14,7 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s: %(name)s: %(levelna
 ######## database setup #########
 def eval_to_db(data_path, conf):
     dataparts = ["apiseq", "methname", "rawcode", "tokens"]
-    """db = UnQLite(filename = './DeepCSKeras/data/database.udb', open_database = True)
+    db = UnQLite(filename = './DeepCSKeras/data/database.udb', open_database = True)
     for part in dataparts:
         source = io.open("./DeepCSKeras/data/codesearchnet/eval.{}.txt".format(part), "r", encoding='utf8', errors='replace')
         lines  = source.readlines()
@@ -32,8 +32,9 @@ def eval_to_db(data_path, conf):
                 data_arr = np.array(data, dtype = np.int)
                 collec.store({str(i): pickle.dumps(data_arr, pickle.HIGHEST_PROTOCOL)})
             print('store time:  {:5.3f}s  <<<<<<<<<<<<<'.format(time.time()-start))
+            print(pickle.dumps(data_arr, pickle.HIGHEST_PROTOCOL))
         source.close()
-    db.close()"""
+    db.close()
        
     # test:
     db = UnQLite(filename = './DeepCSKeras/data/database.udb', open_database = True)
@@ -43,8 +44,8 @@ def eval_to_db(data_path, conf):
         #print(collec.fetch(99)[0])
         start = time.time()
         data = collec.all()
-        print(data[0])
-        print(data[0][0])
+        print(data[collec.last_record_id()])
+        print(data[collec.last_record_id()][0])
         data_arrays = [pickle.loads(d[0].decode(errors='replace')) for d in data]
         print('store time:  {:5.3f}s  <<<<<<<<<<<<<'.format(time.time()-start))
         print(f"len(data_arrays): {len(data_arrays)} | type(data_arrays): {type(data_arrays)} | type(data_arrays)[0]: {type(data_arrays)[0]}")
