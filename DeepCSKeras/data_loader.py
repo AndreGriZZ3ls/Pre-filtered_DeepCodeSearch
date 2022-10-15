4b321c4f11ddc252be23520c724fbad6ac88c36c
André Warnecke <andre.warnecke036@web.de>
2022-10-16 01:08:48 +0200

    save index as .h5 5

--- DeepCSKeras/data_loader.py
+++ DeepCSKeras/data_loader.py
@@ -40,7 +40,7 @@ def load_index_counters(name, word_list, index_path):
     index_file = index_path + self.index_type + '.h5'
     assert os.path.exists(index_file), f"Index file {index_file} not found!"
     h5f     = tables.open_file(index_file, mode = "r")
-    
+    table   = h5f.get_node('/meta')
     
 def save_index(name, index, index_path):
     """db = UnQLite(filename = './DeepCSKeras/data/database.udb', open_database = True)
@@ -61,7 +61,7 @@ def save_index(name, index, index_path):
     atom_v  = tables.Atom.from_dtype(np.array(index.values()[0].values()).dtype)
     filters = tables.Filters(complib = 'blosc', complevel = 5)
     h5f     = tables.open_file(index_file, mode = "w", title = name)
-    table   = h5f.create_table("/", 'readout', IndexMetaData, "index meta data")
+    table   = h5f.create_table("/", 'meta', IndexMetaData, "index meta data")
     meta    = table.row
     keys    = h5f.create_earray(h5f.root, 'keys', atom_k, (0), "key of the counter elements", filters)
     vals    = h5f.create_earray(h5f.root, 'vals', atom_v, (0), "values of the counter elements", filters)
