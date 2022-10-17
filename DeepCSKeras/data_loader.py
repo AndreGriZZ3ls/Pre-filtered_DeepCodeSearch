cde96a9c0af8ba4270a96d6b7825e60c770d4d71
André Warnecke <andre.warnecke036@web.de>
2022-10-17 19:19:55 +0200

    load codebase from sqlite database 16

--- DeepCSKeras/data_loader.py
+++ DeepCSKeras/data_loader.py
@@ -153,8 +153,8 @@ def load_codebase_lines(path, lines, chunk_size, chunk_number = -1):
         curs = conn.cursor()
         cond = "SELECT code FROM codebase WHERE id IN (" + ",".join([str(line) for line in lines]) + ")"
         curs.execute(cond)
-        codebase_lines = *curs.fetchall()
-        codebase_lines = list(codebase_lines)
+        codebase_lines = zip(*curs.fetchall())
+        codebase_lines = list(codebase_lines[0])
         print(codebase_lines)
     else:
         codes = io.open(path, "r", encoding='utf8',errors='replace')
