122e72536fb3e3c7cc475bb0061482163afc3576
André Warnecke <andre.warnecke036@web.de>
2022-10-17 15:13:45 +0200

    load index faster 8

--- main.py
+++ main.py
@@ -223,8 +223,7 @@ if __name__ == '__main__':
                         if word in index: # for each word of the processed query that the index contains: ...
                             cnt += Counter(dict(index[word].most_common(max_filtered))) # sum tf-idf values for each identical line and merge counters in general 
                 else:
-                    counters = data_loader.load_index_counters(index_type, query_list, data_path, max_filtered)
-                    for counter in counters:
+                    for counter in data_loader.load_index_counters(index_type, query_list, data_path, max_filtered):
                         cnt += counter # sum tf-idf values for each identical line and merge counters in general 
                 #print('Time to sum the tf-idf counters:  {:5.3f}s'.format(time.time()-start))
                 ##################################################################################################################
