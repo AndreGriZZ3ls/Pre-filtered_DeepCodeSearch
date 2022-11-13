bdab7acb73d5c37955637888dba0780f16fab1a0
André Warnecke <andre.warnecke036@web.de>
2022-11-13 18:37:03 +0100

    better performance 47

--- DeepCSKeras/data_loader.py
+++ DeepCSKeras/data_loader.py
@@ -69,12 +69,10 @@ def load_index_counters(name, word_list, index_path, max_items):
         keys = h5f.root.keys
         vals = h5f.root.vals
         cond = "|".join(['(word == b"%s")'%w for w in word_list])
-        raw  = meta.where(cond)
-        rows = raw.sort(key = lambda row: -vals[row['pos']])
+        rows = sorted(list(meta.where(cond)), key = lambda row: -vals[row['pos']])
         for i, row in enumerate(rows):
             l = row['len']
             p = row['pos']
-            print(vals[p])
             if i > 1:
                 k = keys[p:p + min(l, max_items)]
                 v = vals[p:p + min(l, max_items)]
